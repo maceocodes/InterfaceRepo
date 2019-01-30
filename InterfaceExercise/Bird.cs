f73e523f3ca117a641f8c5a76b30ea1fce5d99ee
Maceo Lewis <maceo.code@gmail.com>
2019-01-29 22:52:44 -0500

    First Commit

---
+++ InterfaceExercise/Bird.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class Bird : IBird
+    {
+        public void Fly() { }
+    }
+}
